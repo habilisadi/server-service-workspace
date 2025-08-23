65562b2e9751e05ad7d960e27a6ddbe19131bab9
yoosc89 <yoosc89@gmail.com>
2025-08-24 02:18:38 +0900

    1. 초기 세팅

---
+++ src/test/kotlin/com/habilisadi/workspace/WorkspaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.habilisadi.workspace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkspaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
